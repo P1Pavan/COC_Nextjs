d09fffc6d4d0009166d84f0e398494e3b6d433af
Pavan <pavanblackbuck10@gmail.com>
2026-06-09 20:42:55 +0530

    feat: add schemas for message handling and user authentication
    - Added acceptMessageSchema for handling message acceptance.
    - Created messagesSchema to validate message content length.
    - Implemented signInSchema for user sign-in validation.
    - Developed signUpSchema with validations for username, email, and password.
    - Introduced verifySchema for verification code validation.
    - Added dbConnect utility for MongoDB connection management.
    - Implemented resend utility for email sending.
    - Updated package.json to include react-email and resend dependencies.

--- src/Schemas/signUpSchema.ts
+++ src/Schemas/signUpSchema.ts
@@ -1,2 +1,17 @@
-import {z} from "zod"
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "UserName must be atleast 2 character" })
+    .max(20, { message: "UserName must be no more than 20 characters" })
+    .regex(/^[a-zA-Z0-9]+$/, {
+      message: "Username must not contain special character",
+    }),
+
+  email: z.email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be at least 6 characters" }),
+});
 
